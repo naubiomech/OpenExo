85af598e615f2d7857ce795f6b3bbf13e6a9666d
PequenoCoder <lsc234@nau.edu>
2025-03-25 21:08:52 -0700

    Added Changes to Table of Contents

--- Documentation/ReadTheDoc/source/conf.py
+++ Documentation/ReadTheDoc/source/conf.py
@@ -8,13 +8,19 @@
 
 project = 'OpenExo Documentation'
 copyright = '2025, Landon Coonrod, Jack Williams, and Prof. Zach Lerner'
-author = 'Landon Coonrod, Jack Williams, and Prof. Zach Lerner'
+author = 'By Landon Coonrod, Jack Williams, and Prof. Zach Lerner'
 release = '0.1-dev'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx_rtd_theme",  # Not strictly required, but ensures Sphinx sees the theme
+]
+
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +30,19 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
+
+html_theme_options = {
+    # The maximum depth of the global table of contents (on the left).
+    'navigation_depth': 4,
+    
+    # Whether to collapse nested navigation sections.
+    'collapse_navigation': False,
+    
+    # Keeps the navigation pane “fixed” when scrolling.
+    'sticky_navigation': True,
+    
+    # If you only want to show headings for the current page in the left menu, set titles_only=True
+    # 'titles_only': True,
+}
