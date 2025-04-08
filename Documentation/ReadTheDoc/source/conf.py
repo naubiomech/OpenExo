7003a38558509deb03cdf19b2381a8cc0b90d241
PequenoCoder <lsc234@nau.edu>
2025-04-08 14:39:28 -0700

    Added Custom Css

--- Documentation/ReadTheDoc/source/conf.py
+++ Documentation/ReadTheDoc/source/conf.py
@@ -32,6 +32,9 @@ exclude_patterns = []
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
+html_css_files = [
+    'custom.css',
+]
 
 html_theme_options = {
     # The maximum depth of the global table of contents (on the left).
