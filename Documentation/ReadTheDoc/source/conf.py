3a233370de0adf8ced2be9faada6f1b729b8d3ce
PequenoCoder <lsc234@nau.edu>
2025-04-14 14:30:16 -0700

    Done

--- Documentation/ReadTheDoc/source/conf.py
+++ Documentation/ReadTheDoc/source/conf.py
@@ -1,53 +1,31 @@
 import piccolo_theme
 
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'OpenExo Documentation'
 copyright = '2025, Landon Coonrod, Jack Williams, and Prof. Zach Lerner'
 author = 'By Landon Coonrod, Jack Williams, and Prof. Zach Lerner'
 release = '0.1-dev'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
-    "sphinx_rtd_theme",  # Not strictly required, but ensures Sphinx sees the theme
+    # Removed "sphinx_rtd_theme" as it's not needed with piccolo_theme.
 ]
-
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = "piccolo_theme"
+# Remove the following line because piccolo_theme does not provide get_html_theme_path()
+# html_theme_path = [piccolo_theme.get_html_theme_path()]
+
 html_static_path = ['_static']
 html_css_files = [
     'custom.css',
 ]
 
 html_theme_options = {
-    # The maximum depth of the global table of contents (on the left).
-    'navigation_depth': 4,
-    
-    # Whether to collapse nested navigation sections.
-    'collapse_navigation': False,
-    
-    # Keeps the navigation pane “fixed” when scrolling.
-    'sticky_navigation': True,
-    
-    # If you only want to show headings for the current page in the left menu, set titles_only=True
-    # 'titles_only': True,
+    # Add any piccolo_theme specific options here if needed.
 }
