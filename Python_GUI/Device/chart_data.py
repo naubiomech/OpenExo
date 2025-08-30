602cb39d684cb743ae60385e8045b01ec5a352e2
Elliott Kinsley <Pepperdog0007@gmail.com>
2025-08-29 17:13:30 -0700

    Added update param values to chart_data, also added plotting points for the function.

--- Python_GUI/Device/chart_data.py
+++ Python_GUI/Device/chart_data.py
@@ -8,7 +8,28 @@ class ChartData:
         self.leftSet = 0.0
         self.rightFsr = 0.0
         self.leftFsr = 0.0
+        self.param_names = []  # List to store parameter names
+        self.param_values = []  # List to store parameter values
+        self.num_params = 0 # Number of parameters
 
+    #function to update the parameter names and number of parameters
+    def updateNames(
+        self, 
+        param_names, 
+        num_params
+    ):
+        self.param_names = param_names
+        self.num_params = num_params
+
+    # function to update the parameter values using the parameter list data streaming protocol
+    def updateParamValues(
+        self,
+        param_values
+    ):
+        self.param_values = param_values
+
+    # old function to update the values of the chart data
+    # this is not used anymore, but is kept for reference
     def updateValues(
         self,
         rightTorque,
@@ -18,8 +39,8 @@ class ChartData:
         rightSet,
         leftSet,
         rightFsr,
-        leftFsr,
-    ):
+        leftFsr
+    ):           
         self.rightTorque = rightTorque
         self.rightState = rightState
         self.leftTorque = leftTorque
