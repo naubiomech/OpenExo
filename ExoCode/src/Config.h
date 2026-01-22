f49d3ad470b9f6baba8e92f36e210f925422b599
ssh252 <ssh252@nau.edu>
2026-01-22 14:07:34 -0700

    Reverted UART plotting to main. Changed Battery Sensor from INA219 to INA260

--- ExoCode/src/Config.h
+++ ExoCode/src/Config.h
@@ -24,7 +24,7 @@
     //Update the define below to match the board version being used
     #define BOARD_VERSION AK_Board_V0_5_1 //Update this define to match the board version being used
 
-	#define BATTERY_SENSOR 219 //Set it to 0 to disable, 219 to use INA219, 260 to use INA260, 3 to use the OpenExo Board 0.5.1 Mark 3's onboard voltage divider
+	#define BATTERY_SENSOR 260 //Set it to 0 to disable, 219 to use INA219, 260 to use INA260, 3 to use the OpenExo Board 0.5.1 Mark 3's onboard voltage divider
 	#define CRITICAL_BATT_VAL 18 //In volts. Battery voltage below this will trigger the low battery warning in the GUI.
 	#define RESISTOR_1 47000 //Set it to the measured resistance of R1 on the OpenExo Board 0.5.1 Mark 3
 	#define RESISTOR_2 4700 //Set it to the measured resistance of R2 on the OpenExo Board 0.5.1 Mark 3
